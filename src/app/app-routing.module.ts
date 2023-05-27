4ee23bfcacca6d6bf2a6b054ca3c1d60ac658116
SammayadeepPal <sammayadeep@divii.com>
2023-05-27 20:37:55 +0530

    add

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
